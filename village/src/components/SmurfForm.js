799eef0ace7e36e800732ea99cf8b5004a1c9600
Danielle B <danibaxx@hotmail.com>
2019-06-30 20:04:13 -0600

    updated App.js to render information, started to add in post on SmurfForm.js

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,6 +14,13 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
+    axios.post('http://localhost:3333/smurfs', event)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log("Error Loading....", error)
+      });
 
     this.setState({
       name: '',
